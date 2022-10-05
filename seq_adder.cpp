f69ec261f85afe1d5f450224440d7ae839a965dd
huseyinmen <huseyin@gronska.org>
2022-10-05 19:52:26 +0200

    removing unused files

--- seq_adder.cpp
+++ seq_adder.cpp
@@ -1,121 +1,139 @@
 #include <iostream>
 
 using namespace std;
-class kol{
-    public:
-        int deger;
-        kol *alt_kol;
-        };
-class branch{
-    public:
-        kol *sonraki=NULL;
-        kol *pre=NULL;
-        void basa_ekle(int add);
-        void sona_ekle(int add);
-        void araya_ekle(int add,int ara);
-        void sirali_ekle(int add);
-        void yazdir();
-        void veri_sil(int del);
-        void sil();
+class kol
+{
+public:
+    int deger;
+    kol *alt_kol;
 };
-void branch::basa_ekle(int add){
-    kol *a=new kol;
-    a->deger=add;
-    a->alt_kol=sonraki;
-    sonraki=a;
+class branch
+{
+public:
+    kol *my_data = NULL;
+    kol *iter = NULL;
+    void basa_ekle(int add);
+    void sona_ekle(int add);
+    void araya_ekle(int add, int ara);
+    void sirali_ekle(int add);
+    void yazdir();
+    void veri_sil(int del);
+    void sil();
+};
+void branch::basa_ekle(int add)
+{
+    kol *a = new kol;
+    a->deger = add;
+    a->alt_kol = my_data;
+    my_data = a;
 }
-void branch::sirali_ekle(int add){
-    pre=sonraki;
-    if(sonraki==NULL){   // yapi bos ise basa ekleme yapar
-        cout<<"basa eklendi"<<endl;
-        basa_ekle(add);    
+void branch::sirali_ekle(int add)
+{
+    iter = my_data;
+    if (my_data == NULL || my_data->deger > add)
+    { // yapi bos ise basa ekleme yapar
+        cout << "basa eklendi" << endl;
+        basa_ekle(add);
         return;
     }
-    if (sonraki->deger > add) // eklemek istedigim deger ilk degerden kucuk ise yine basa ekleme yapar
+
+    while (iter->alt_kol->deger < add)
+    {
+
+        iter = iter->alt_kol;
+        if (iter->alt_kol == NULL)
+        {
+            sona_ekle(add);
+            return;
+        }
+    }
+    araya_ekle(add, iter->alt_kol->deger);
+}
+void branch::sona_ekle(int add)
+{
+    kol *a = new kol;
+    a->deger = add;
+    a->alt_kol = NULL;
+    if (my_data == NULL)
     {
         basa_ekle(add);
         return;
     }
-    //eger eklemek itedigim deger kendinden buyuk ilk degerin onune eklenecek 
-    while (pre->alt_kol->deger < add){
-            
-            pre=pre->alt_kol;
-            if(pre->alt_kol ==NULL){
-                sona_ekle(add);
-                return;}
+    else
+    {
+        iter = my_data;
+        while (iter->alt_kol != NULL)
+        {
+            iter = iter->alt_kol;
         }
-        araya_ekle(add,pre->alt_kol->deger);
+        iter->alt_kol = a;
+    }
 }
-void branch::sona_ekle(int add){
-    kol *a=new kol;
-    a->deger=add;
-    a->alt_kol=NULL;
-    if(sonraki==NULL){
+void branch::araya_ekle(int add, int ara)
+{
+    kol *a = new kol;
+    a->deger = add;
+    if (my_data == NULL)
         basa_ekle(add);
-        return;
+    else
+    {
+        iter = my_data;
+        while (iter->alt_kol->deger != ara)
+        {
+            iter = iter->alt_kol;
         }
-    else{
-        pre=sonraki;
-        while(pre->alt_kol != NULL){
-            pre=pre->alt_kol;
-        }
-        pre->alt_kol=a;
-}    
-}
-void branch::araya_ekle(int add,int ara){
-    kol *a=new kol;
-    a->deger=add;
-    if(sonraki==NULL)
-        cout<<"araya deger eklemeden once basa veya son a deger ekleyin";
-    else{
-        pre=sonraki;
-        while(pre->alt_kol->deger != ara) {
-            pre=pre->alt_kol;
-        }
-        kol *on=new kol;
-        on=pre->alt_kol;
-        pre->alt_kol=a;
-        a->alt_kol=on;
+        kol *on = new kol;
+        on = iter->alt_kol;
+        iter->alt_kol = a;
+        a->alt_kol = on;
     }
 }
-void branch::yazdir(){
-    kol *a=new kol();
-    a=sonraki;
-    while(a->alt_kol!=NULL){
-        cout<<a->deger<<endl;  
-        a=a->alt_kol;
+void branch::yazdir()
+{
+    kol *a = new kol();
+    a = my_data;
+    while (a->alt_kol != NULL)
+    {
+        cout << a->deger << endl;
+        a = a->alt_kol;
     }
-    cout<<a->deger<<endl;
-    //cout<<sonraki->deger<<endl;
+    cout << a->deger << endl;
+    // cout<<my_data->deger<<endl;
 }
-void branch::sil(){
-    pre=sonraki->alt_kol;
-    delete sonraki;
-    sonraki=pre;
+void branch::sil()
+{
+    iter = my_data->alt_kol;
+    delete my_data;
+    my_data = iter;
 }
-void branch::veri_sil(int del){
-    pre=sonraki;
-    kol *temp=new kol;
-    if(del==sonraki->deger){
+void branch::veri_sil(int del)
+{
+    iter = my_data;
+    kol *temp = new kol;
+    if (del == my_data->deger)
+    {
         sil();
         return;
     }
-    else {
-        while(pre->alt_kol !=NULL && pre->alt_kol->deger != del ){
-            pre=pre->alt_kol;
-        }
-        
-        if(pre->alt_kol==NULL)
+    else
+    {
+        while (iter->alt_kol != NULL && iter->alt_kol->deger != del)
         {
-            cout<<"31 i dugum icinde bulunamadim"<<endl;
+            iter = iter->alt_kol;
+        }
+
+        if (iter->alt_kol == NULL)
+        {
+            cout << "31 i dugum icinde bulunamadim" << endl;
             return;
         }
-        temp=pre->alt_kol;
-        pre->alt_kol=pre->alt_kol->alt_kol;
+        temp = iter->alt_kol;
+        iter->alt_kol = iter->alt_kol->alt_kol;
         delete temp;
-        }
+    }
 }
-int main(){
+int main()
+{
     branch vektor;
     // vektor.sona_ekle(65);//sonrasina ekleyecegi herhangi bir degeri olmadigi icin basa ekleme yapti
     // vektor.sona_ekle(34);
@@ -128,7 +146,7 @@ int main(){
     // cout<<"araya ekleme yapildiktan sonra"<<endl;
     // vektor.araya_ekle(99,56);
     // vektor.yazdir();
-    cout<<"siraya gore ekleme yapildiktan sonra"<<endl;
+    cout << "siraya gore ekleme yapildiktan sonra" << endl;
     vektor.sirali_ekle(34);
     vektor.sirali_ekle(30);
     vektor.sirali_ekle(22);
@@ -137,7 +155,7 @@ int main(){
     vektor.sirali_ekle(33);
     vektor.sirali_ekle(56);
     vektor.yazdir();
-    cout<<"belli degeri silme yapildiktan sonra"<<endl;
+    cout << "belli degeri silme yapildiktan sonra" << endl;
     vektor.veri_sil(35);
     vektor.yazdir();
 
