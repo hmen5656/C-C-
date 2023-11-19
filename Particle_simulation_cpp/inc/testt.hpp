4310605e9dfd49eb43715cba1235991015c744f2
huseyinmen <huseyin@gronska.org>
2023-11-19 18:25:02 +0100

    test

---
+++ Particle_simulation_cpp/inc/testt.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace template_project {
+
+namespace common_utils {
+
+    class Testt 
+    {
+    public:
+        Testt ();
+        ~Testt ();
+
+    private:
+
+    };
+
+}  // namespace common_utils
+
+}  // namespace template_project
---
+++ Particle_simulation_cpp/src/testt.cpp
@@ -0,0 +1,19 @@
+#include "testt.hpp"
+
+namespace template_project {
+
+namespace common_utils {
+
+Testt::Testt()
+{
+
+}
+
+Testt::~Testt()
+{
+
+}
+
+}  // namespace common_utils
+
+}  // namespace template_project
